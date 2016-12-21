d60011d1fb45e8b316f550abaac756dd5e587fde
JulianSansat <kerarchie@yahoo.com.ar>
2016-12-21 11:18:59 -0300

    remove vertex method

--- lib/graph.rb
+++ lib/graph.rb
@@ -15,6 +15,9 @@ class Graph
         vertex.graph = self
     end
 
+    def remove_vertex(vertex)
+        vertexs.delete(vertex)
+    end
 end
 
 @graph = Graph.new
@@ -24,10 +27,16 @@ end
 @graph.add_vertex(vertex3 = Vertex.new("v3"))
 @graph.vertexs[0].add_edge(vertex2, 10)
 @graph.vertexs[1].add_edge(vertex3, 15)
+puts "imprime vertices:"
 @graph.vertexs.each do |vertex|
     puts vertex.edges
 end
 @graph.vertexs[0].remove_edge(vertex0.edges.first)
+#@graph.remove_vertex(vertex1)
+puts "imprime vertices:"
 @graph.vertexs.each do |vertex|
     puts vertex.edges
 end
+
+
+
